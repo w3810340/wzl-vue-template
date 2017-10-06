14c7e34cf7641b493422ea072c76860c20212329
Leo Pham <hongthaipro@gmail.com>
2017-10-06 22:33:22 +0700

    add example

---
+++ example/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Capacity from 'react-capacity-file';
+
+
+export default class Test extends React.Component {
+    render() {
+        return (
+            <Capacity value="1024" />
+        )
+    }
+}
